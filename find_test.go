4962b408f877b2a7de2347b2567709f45a1120f6
Leon <a.li.devtty@gmail.com>
2014-10-30 21:50:45 +0800

    add more tests

--- find_test.go
+++ find_test.go
@@ -16,3 +16,28 @@ func Test_Find_1(t *testing.T) {
 		t.Error("Failed")
 	}
 }
+
+func Test_Find_2(t *testing.T) {
+	hf, _ := os.Open(testfile)
+	defer hf.Close()
+	path := "html:1//.table//.table2"
+	tags := NewPath(path)
+	t.Logf("%#V\n", tags)
+	nodes, _ := Find(tags, hf)
+	if len(nodes) != 2 {
+		t.Error("Failed")
+	}
+}
+
+func Test_Find_3(t *testing.T) {
+	hf, _ := os.Open(testfile)
+	defer hf.Close()
+	path := "//:1/:2//a"
+	tags := NewPath(path)
+	t.Logf("%#V\n", tags)
+	nodes, _ := Find(tags, hf)
+	t.Logf("%#V\n", nodes)
+	if len(nodes) != 2 {
+		t.Error("Failed")
+	}
+}
