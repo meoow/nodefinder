6ee3f0b6252e05f53178e4f033cfd0e404f85533
Leon Belmont <a.li.devtty@gmail.com>
2015-05-24 12:05:21 +0800

    add cmd tool

---
+++ cmd/findnode.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/meoow/nodefinder"
+	"golang.org/x/net/html"
+	"os"
+	"strings"
+)
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "%s -i HTML PATH [PATH ...]\n", os.Args[0])
+	flag.PrintDefaults()
+}
+
+var input string
+
+func init() {
+	flag.Usage = usage
+	flag.StringVar(&input, "i", "", "input file")
+}
+
+func main() {
+	flag.Parse()
+
+	if input == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if fi, err := os.Stat(input); err != nil || fi.IsDir() {
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		} else {
+			fmt.Fprintf(os.Stderr, "%s is not a file", fi.Name())
+		}
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	var xpath string
+	if flag.NArg() > 0 {
+		xpath = strings.Join(flag.Args(), ":::")
+	}
+
+	fh, err := os.Open(input)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	tags := nodefinder.NewPath(xpath)
+	nodes, err := nodefinder.Find(tags, fh)
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	for _, i := range nodes {
+		html.Render(os.Stdout, i)
+	}
+}
